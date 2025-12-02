8823019b8ea40538ebc6c35e427949f4cc1e4032
Xhhff <xuhouhua2023@outlook.com>
2025-12-02 22:11:06 +0800

    Delete noname directory

--- noname/library/experimental/index.js
+++
@@ -1,8 +0,0 @@
-import { Uninstantable } from "../../util/index.js";
-
-import { ExperimentalSymbol } from "./symbol.js";
-
-export class Experimental extends Uninstantable {
-	static symbol = ExperimentalSymbol;
-	static symbols = ExperimentalSymbol;
-}
--- noname/library/experimental/symbol.js
+++
@@ -1,5 +0,0 @@
-import { Uninstantable } from "../../util/index.js";
-
-export class ExperimentalSymbol extends Uninstantable {
-	static itemType = Symbol("noname.experimental.itemType");
-}
